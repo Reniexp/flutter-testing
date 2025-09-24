cfd42017ebb9fba15186ba5731ae5460ac63caa1
Reni Ferreira Drie <renirogete.exp@gmail.com>
2025-09-24 12:45:52 -0300

    sub, mult and group testing

--- lib/maths_util.dart
+++ lib/maths_util.dart
@@ -4,5 +4,8 @@ int add(int a, int b) {
 
 int substract(int a, int b) {
   return a - b;
+}
 
+int multiply(int a, int b) {
+  return a * b;
 }
--- test/maths_util_test.dart
+++ test/maths_util_test.dart
@@ -3,18 +3,42 @@ import 'package:test/test.dart';
 
 
 void main() {
-  test('check for two number addition', (){
+  group('Maths Util -', () {
+    test('check for two number addition', (){
       // Arrange
       var a = 10;
       var b = 10;
 
-      var sum = add(a, b);
-
       // Act
-
+      var sum = add(a, b);
 
       //Assert
       expect(sum, 20);
 
   });
+  test('check for two number substraction', (){
+    // Arrange
+    var a = 10;
+    var b = 10;
+
+    // Act
+    var sub = substract(a, b);
+
+    // Assert
+    expect(sub, 0);
+
+  });
+  test('check for two number multiplication', (){
+    // Arrange
+    var a = 10;
+    var b = 10;
+
+    // Act
+    var mult = multiply(a, b);
+
+    // Assert
+    expect(mult, 100);
+});
+  });
+  
 }
