07167725b67aea944acd30f8be48c1e1b54558d0
Reni Ferreira Drie <renirogete.exp@gmail.com>
2025-09-26 12:25:34 -0300

    Added widgets and some tests for then

--- LOGIN_EXAMPLE/login_example_1/lib/login_screen.dart
+++ LOGIN_EXAMPLE/login_example_1/lib/login_screen.dart
@@ -8,11 +8,29 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
+    return Scaffold(
+      appBar: AppBar(
       title: const Text("Login"),
       ),
+      body: Column(
+        children: <Widget>[
+          TextFormField(
+            controller: _emailController,
+            key: const ValueKey("email_id"),
+            decoration: const InputDecoration(hintText: "Enter Email Id"),
+          ),
+          TextFormField(
+            controller: _passwordController,
+            key: const ValueKey("password"),
+            decoration: const InputDecoration(hintText: "Enter Password"),
+            )
+        ]
+      )
     );
   }
 }
--- LOGIN_EXAMPLE/login_example_1/test/login_screen_test.dart
+++ LOGIN_EXAMPLE/login_example_1/test/login_screen_test.dart
@@ -15,4 +15,37 @@ void main() {
     // Assert
     expect(tittle, findsOneWidget); // findsOneWidget ou findsWidgets busca todos/findsNWidgets
   });
+
+  // email
+
+  testWidgets("Should have one text field form to collect user email id",
+      (WidgetTester tester) async {
+
+    // Arrange
+    await tester.pumpWidget(const MaterialApp(
+      home: LoginScreen(),
+    ));
+  
+    // Act
+    Finder userNameTextField = find.byKey(const ValueKey("email_id"));
+
+    // Assert
+    expect(userNameTextField, findsOneWidget); 
+    });
+
+
+    testWidgets("Should have one text field form to collect Password",
+      (WidgetTester tester) async {
+
+    // Arrange
+    await tester.pumpWidget(const MaterialApp(
+      home: LoginScreen(),
+    ));
+  
+    // Act
+    Finder passwordTextField = find.byKey(const ValueKey("password"));
+
+    // Assert
+    expect(passwordTextField, findsOneWidget); 
+    });
 }
