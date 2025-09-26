127ed5c0532c5226ea237fbb714f044f8e7af70d
Reni Ferreira Drie <renirogete.exp@gmail.com>
2025-09-26 14:11:44 -0300

    Final Widget Testing

--- LOGIN_EXAMPLE/login_example_1/test/login_screen_test.dart
+++ LOGIN_EXAMPLE/login_example_1/test/login_screen_test.dart
@@ -83,4 +83,32 @@ void main() {
         // Assert
         expect(errorTexts, findsNWidgets(2));
     });
+
+    testWidgets("Should submit form when user email id & password is valid", 
+        (WidgetTester tester) async {
+
+      // Arrange
+      await tester.pumpWidget(const MaterialApp(
+        home: LoginScreen(),
+      ));
+
+      // Act  
+      // entrar com o email e senha
+      Finder userNameTextField = find.byKey(const ValueKey("email_id"));
+      Finder passwordTextField = find.byKey(const ValueKey("password"));
+      await tester.enterText(userNameTextField, "richa@gmail.com");
+      await tester.enterText(passwordTextField, "password");
+
+      // procurar o botao e clicar
+      Finder loginButton = find.byType(ElevatedButton);
+      await tester.tap(loginButton);
+      await tester.pumpAndSettle();
+
+      // procurar as mensagens de erro
+      Finder errorTexts = find.text("Require field");
+
+      // Assert
+      expect(errorTexts, findsNothing);
+
+    });
 }
