0fcc0366ce848774c994115035f905f3376c81c2
Reni Ferreira Drie <renirogete.exp@gmail.com>
2025-09-26 11:30:51 -0300

    fix

--- LOGIN_EXAMPLE/login_example_1/test/validator_test.dart
+++ LOGIN_EXAMPLE/login_example_1/test/validator_test.dart
@@ -3,34 +3,25 @@ import 'package:login_example_1/validator.dart';
 
 void main() {
   test('validate for empty email id', () {
-
     // Arrange & Act
     var result = Validator.validateEmail('');
-
     // Assert
     expect(result, "Require field");
-
   });
 
 
   test('validate for invalid email id', () {
-
     // Arrange & Act
     var result = Validator.validateEmail('gdsfdhasd');
-
     // Assert
     expect(result, "Please enter a valid email id");
-
   });
 
   test('validate for valid email id', () {
-
       // Arrange & Act
       var result = Validator.validateEmail('abc@gmail.com');
-
       // Assert
       expect(result, null);
-
     });
   
 }
