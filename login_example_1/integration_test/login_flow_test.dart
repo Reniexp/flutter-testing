ee950058843c5b7f09acb344ab7e7f922dd9adef
Reni Ferreira <renirogete.exp@gmail.com>
2025-10-02 22:49:40 -0300

    login flow integration test

---
+++ login_example_1/integration_test/login_flow_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:login_example_1/login_screen.dart';
+
+void main() {
+  group("Login Flow Test", (){
+    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+    testWidgets(
+      "Should show home screen when user taps on login button after entering valid email id & password", (WidgetTester tester) async {
+      // Arrange
+      await tester.pumpWidget(const MaterialApp(
+        home: LoginScreen(),
+      ));
+
+      // Act  
+      // entrar com o email e senha
+      Finder userNameTextField = find.byKey(const ValueKey("email_id"));
+      Finder passwordTextField = find.byKey(const ValueKey("password"));
+      await tester.enterText(userNameTextField, "richa@gmail.com");
+      await tester.enterText(passwordTextField, "password");
+
+      // procurar o botao e clicar
+      Finder loginButton = find.byType(ElevatedButton);
+      await tester.tap(loginButton);
+      await tester.pumpAndSettle();
+
+      Finder welcomeText = find.byType(Text);
+
+
+      // Assert
+      expect(welcomeText, findsOneWidget);
+    });
+  });
+}
